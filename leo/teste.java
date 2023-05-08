eced72e597b1019f18fd7721d9bdd2f853572e93
Leonardodahoramedeiros <126834012+Leonardodahoramedeiros@users.noreply.github.com>
2023-05-08 20:59:41 -0300

    completo

--- leo/teste.java
+++ leo/teste.java
@@ -2,6 +2,8 @@ package leo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class teste {
 
@@ -16,7 +18,29 @@ public class teste {
 			Connection conn = DriverManager.getConnection(url,usuario, senha);
 			System.out.println(conn.isValid(0));
 			
+			//monstar a instrução para montar o banco de dados 	
+			String sql = "select * from clientes where nome = ?";
+			
+			//prepara o comando (preparedStatement
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			//atribuir os valoes aos parametros da instrução do SQL
+			ps.setString(1, "Pedro de Lara");
+			
+			//executa o sql e recebe o obejto do tipo Resultset
+			ResultSet rs = ps.executeQuery();
+			System.out.println(rs);
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String nome = rs.getString("nome");
+				String email = rs.getString("email");
+				System.out.println(id + " " + nome + " " + email +" ");
+			}
+			
+			
+			
 		}catch(Exception e) {
+			System.out.println(e);
 			
 			
 		}
