e029f5af33b6ba5520b898c99a7e050415d0c0bd
Leonardodahoramedeiros <126834012+Leonardodahoramedeiros@users.noreply.github.com>
2023-05-08 21:39:06 -0300

    new

--- leo/teste.java
+++ leo/teste.java
@@ -3,7 +3,6 @@ package leo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class teste {
 
@@ -18,6 +17,8 @@ public class teste {
 			Connection conn = DriverManager.getConnection(url,usuario, senha);
 			System.out.println(conn.isValid(0));
 			
+			
+			/* exemplo para select
 			//monstar a instrução para montar o banco de dados 	
 			String sql = "select * from clientes where nome = ?";
 			
@@ -36,8 +37,40 @@ public class teste {
 				String email = rs.getString("email");
 				System.out.println(id + " " + nome + " " + email +" ");
 			}
+			*/
+			//=================================
+			/*exemplo para insert*/
+			
+			/*
+			String sql = "insert into clientes(id, cpf, nome, email) values(?,?,?,?)";
+			
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, 3);
+			ps.setString(2, "101010");
+			ps.setString(3, "Sergio Malandro");
+			ps.setString(4, "malandro@mail.com");
+			
+			int retorno = ps.executeUpdate();
+			System.out.println(retorno);
+			*/
 			
 			
+			//Exemplo do update
+			/*String sql = "update clientes set email = ? where cpf = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, "pedrao@mail.com");
+			ps.setString(2, "888");
+			int retorno = ps.executeUpdate();
+			if(retorno > 0) {
+				System.out.println("dado atualizado");
+			}
+			*/
+			
+			String sql = "delete from clientes where cpf = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, "888");
+			ps.executeUpdate();
+	
 			
 		}catch(Exception e) {
 			System.out.println(e);
